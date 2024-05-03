59c3fd0d88d5818634a02f8be90ad17919ac846d
DanThePanMan <danielchenschw@gmail.com>
2024-05-03 15:09:32 -0400

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import tw from 'twrnc';
+
+
+export default function App() {
+  return (
+    <View style={tw`bg-red-200 flex flex-auto justify-center items-center`}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [["../native-todo/**/*.{html,js}"],],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
